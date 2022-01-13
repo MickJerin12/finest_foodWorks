be8d9be4eba95bd0524c92a1dc2c8e0177cf6ca9
MickJerin12 <s.mickjerin@outlook.com>
2022-01-13 21:47:15 +0530

    added redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async redirect() {
+    return [
+      {
+        source: "/",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
