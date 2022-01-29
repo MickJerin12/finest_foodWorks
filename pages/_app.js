2d542f3120bb84d58f58d2a748fa44e81246cf51
MickJerin12 <s.mickjerin@outlook.com>
2022-01-29 21:36:04 +0530

    CodeBase change

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Finest Food works</title>
+        <meta name="description" content="Finest Food Works"></meta>
+        <meta name="robots" content="index, follow" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
